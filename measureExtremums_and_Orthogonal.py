adc4698b88ef0bf6d2a44e3bfcfab3d06531dca7
Sean Hendryx <seanmhendryx@gmail.com>
2017-05-19 17:35:06 -0700

    measureExtremums.py function implemented

--- measureExtremums_and_Orthogonal.py
+++ measureExtremums_and_Orthogonal.py
@@ -4,8 +4,11 @@
 
 # Written by Sean Hendryx adapted from http://stackoverflow.com/questions/31667070/max-distance-between-2-points-in-a-data-set-and-identifying-the-points
 
-from numpy import random, nanmax, argmax, unravel_index
+from numpy import random, nanmax, argmax, unravel_index, absolute
 from scipy.spatial.distance import pdist, squareform
+import random
+import math
+import numpy as np
 
 def measureExtremums(points, orthogonal = False):
     """
@@ -25,4 +28,95 @@ def measureExtremums(points, orthogonal = False):
         # Get two furthest points and return them:
         return maxDist, points[I_row], points[I_col]
     else:
-        orthDist = 
+        #in dev:
+        p1, p2 = points[I_row], points[I_col]
+        x1 = p1[0]
+        x2 = p2[0]
+        y1 = p1[1]
+        y2 = p2[1]
+        # Make line end-points into vector:
+        #http://stackoverflow.com/questions/3838319/how-can-i-check-if-a-point-is-below-a-line-or-not
+        v1 = {'x':x2-x1,'y': y2-y1}
+        #
+        oneHalf = []
+        secondHalf = []
+        # split the points in half (one set on each side of the major axis):
+        #compute cross product to test if a given point is on the same side of the line as the reference point or not:
+        referencePoint = points[0]
+        #http://stackoverflow.com/questions/13724405/determining-which-side-of-a-line-is-on-given-3rd-point-indicating-desired-side
+        for point in points:
+            #test = np.dot(np.cross((p1 * p2), (p1 * referencePoint)), np.cross((p1 * p2), (p1 * point)))
+            xA = point[0]
+            yA = point[1]
+            v2 = {'x':x2-xA, 'y':y2-yA}
+            test = v1['x']*v2['y'] - v1['y']*v2['x']
+            print "test: ", test
+            if test > 0.:
+                oneHalf.append(point)
+            else:
+                secondHalf.append(point)
+        #
+        #compute furthest point in each half from line:
+        furthestDist_1 = 0.0
+        for point in oneHalf:
+            x0 = point[0]
+            y0 = point[1]
+            dist_i = absolute((y2 - y1) * x0 - (x2 - x1) * y0 + x2 * y1 - y2 * x1) / ((y2 - y1)**2. + (x2 - x1)**2.)**.5
+            if dist_i > furthestDist_1:
+                furthestDist_1 = dist_i
+                furthestOrthoPoint_1 = point
+        #
+        furthestDist_2 = 0.0
+        for point in secondHalf:
+            x0 = point[0]
+            y0 = point[1]
+            dist_i = absolute((y2 - y1) * x0 - (x2 - x1) * y0 + x2 * y1 - y2 * x1) / ((y2 - y1)**2. + (x2 - x1)**2.)**.5
+            if dist_i > furthestDist_2:
+                furthestDist_2 = dist_i
+                furthestOrthoPoint_2 = point
+        #
+        orthoDist = furthestDist_1 + furthestDist_2
+        return maxDist, points[I_row], points[I_col], orthoDist, furthestOrthoPoint_1, furthestOrthoPoint_2
+
+
+
+def generateSamplePoints(numPoints, shape = "circle-ish"):
+    if shape == "circle-ish":
+        # radius of the circle
+        circle_r = 10
+        # center of the circle (x, y)
+        circle_x = 5
+        circle_y = 7
+        #
+        points = []
+        for i in range(numPoints):
+            # random angle
+            alpha = 2 * math.pi * random.random()
+            # random radius
+            r = circle_r * random.random()
+            # calculating coordinates
+            x = r * math.cos(alpha) + circle_x
+            y = r * math.sin(alpha) + circle_y
+            #
+            print "Random point ", (x, y)
+            point = [x,y]
+            points.append(point)
+            #
+        #
+        print "All points:\n", points
+        return points
+
+
+#test:
+import matplotlib.pyplot as plt
+import numpy as np
+points = np.asarray((generateSamplePoints(100)))
+distance, p1, p2, orthoDist, furthestOrthoPoint_1, furthestOrthoPoint_2 = measureExtremums(points, orthogonal = True)
+
+plt.plot([p1[0], p2[0]], [p1[1], p2[1]], color='red')
+plt.scatter(points[:,0], points[:,1])
+plt.scatter([furthestOrthoPoint_1[0], furthestOrthoPoint_2[0]], [furthestOrthoPoint_1[1], furthestOrthoPoint_2[1]], s = 40, color='green')
+plt.scatter([p1[0], p2[0]], [p1[1], p2[1]], s = 40, color='orange')
+#plt.scatter(X,Y1,color='red')
+plt.show()
+
